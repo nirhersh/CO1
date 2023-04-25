279a3918c9233317531a76f4fd247a88b81cbfbc
nirhersh <nirherscovici1@gmail.com>
2023-04-25 15:53:08 +0300

    ex1
    Co-authored-by: LiamK-Technion <LiamK-Technion@users.noreply.github.com>

--- ex1.asm
+++ ex1.asm
@@ -2,4 +2,17 @@
 
 .section .text
 _start:
-#your code here
+    movb $0, Bool
+    movq $0, %rcx # rcx is the iteration number
+    movq num, %rax
+loop_HW1:
+    rol %rax
+    inc %rcx
+    cmpq $64, %rcx
+    je finish_HW1
+    jb inc_HW1
+    jmp loop_HW1
+inc_HW1:
+    incb Bool
+    jmp loop_HW1
+finish_HW1: 
