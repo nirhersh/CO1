44c2863d0d55e373553237b35a85337ee57c4337
Liam Koshrovski <liamk@campus.technion.ac.il>
2023-05-02 21:38:29 +0300

    implemented ex2 and ex3
    Co-authored-by: nirhersh <nirhersh@users.noreply.github.com>

--- asm_files/ex2.asm
+++ asm_files/ex2.asm
@@ -6,12 +6,13 @@ _start:
     movq $destination, %rdx # rdx hold the address of destination
     movq $source, %rbx # rbx hold the address of source
     cmpl $0, num
-    jge _positive_HW1
+    je _finish_HW1
+    jg _positive_HW1
     jl _negative_HW1
 
 _positive_HW1:
-    #cmpq %rbx, %rdx # check if dest is bigger than source
-    #jge _source_before_dest_HW1
+    cmpq %rbx, %rdx # check if dest is bigger than source
+    jge _source_before_dest_HW1
 _loop_1_HW1:
     cmpl num, %ecx
     je _finish_HW1
@@ -23,13 +24,14 @@ _loop_1_HW1:
 
 _source_before_dest_HW1:
     movl num, %ecx
-    cmpl $0, %ecx
+    dec %ecx
 _loop_2_HW1:
-    je _finish_HW1
     movb (%rbx, %rcx), %al
     leaq (%rdx, %rcx), %r9 # r9 holds the address to copy
     movb %al, (%r9)
     dec %ecx
+    cmpl $-1, %ecx
+    je _finish_HW1
     jmp _loop_2_HW1
 
 _negative_HW1:
--- asm_files/ex3.asm
+++ asm_files/ex3.asm
@@ -2,4 +2,69 @@
 
 .section .text
 _start:
-#your code here
+    movq $array1, %rax # %rax is the address of current element in array 1
+    movq $array2, %rbx # %rbx is the address of current element in array 2
+    movq $mergedArray, %rdi # %rdi is the current element in the merged array
+    movq $0, %rcx # counter
+
+_loop_HW1:
+    movl (%rax), %esp #%esp is the element in array 1
+    movl (%rbx), %ebp # ebp is the element in array 2
+    cmpl $0, %esp
+    je _array1_finished_HW1
+    jmp _run_through_array1_HW1
+
+_continue_HW1:
+    cmpl $0, %ebp
+    jne _run_through_array2_HW1
+
+_ready_to_compare_HW1:
+    lea (%rdi, %rcx, 4), %r10
+    cmpl %esp, %ebp
+    ja _copy_from_array2_HW1
+    jb _copy_from_array1_HW1
+    # array1=array2
+    movl %esp, (%r10)
+    lea 4(%rax), %rax
+    lea 4(%rbx), %rbx
+    inc %rcx
+    jmp _loop_HW1
+
+_copy_from_array1_HW1:
+    movl %esp, (%r10)
+    inc %rcx
+    lea 4(%rax), %rax
+    jmp _loop_HW1
+
+_copy_from_array2_HW1:
+    movl %ebp, (%r10)
+    inc %rcx
+    lea 4(%rbx), %rbx
+    jmp _loop_HW1
+    
+_array1_finished_HW1:
+    cmpl $0, %ebp
+    je _finish_HW1
+    jmp _continue_HW1
+
+
+_run_through_array1_HW1:
+    lea 4(%rax), %r15
+    movl (%r15), %esi
+    cmpl %esi, %esp
+    jne _continue_HW1
+    movq %r15, %rax
+    jmp _run_through_array1_HW1
+    
+_run_through_array2_HW1:
+    lea 4(%rbx), %r15
+    movl (%r15), %esi
+    cmpl %esi, %ebp
+    jne _ready_to_compare_HW1
+    movq %r15, %rbx
+    jmp _run_through_array2_HW1
+
+_finish_HW1:
+    lea (%rdi, %rcx, 4), %r10
+    movl $0, (%r10)
+    
